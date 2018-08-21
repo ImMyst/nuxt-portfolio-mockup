df9ec4457a8a33b407c279adad9f42ea063ff56f
ImMyst <titouan.galvani@laposte.net>
2018-08-21 19:57:32 +0200

    Add markup for Skills page

--- pages/skills/index.vue
+++ pages/skills/index.vue
@@ -1,5 +1,29 @@
 <template>
+<section class="skills">
   <h2>Skills</h2>
+  <p class="lead">Programming Languages & Tools</p>
+  <div class="icons">
+    <i class="fab fa-html5 fa-3x"></i>
+    <i class="fab fa-css3-alt fa-3x"></i>
+    <i class="fab fa-js-square fa-3x"></i>
+    <i class="fab fa-angular fa-3x"></i>
+    <i class="fab fa-react fa-3x"></i>
+    <i class="fab fa-node-js fa-3x"></i>
+    <i class="fab fa-sass fa-3x"></i>
+    <i class="fab fa-less fa-3x"></i>
+    <i class="fab fa-wordpress fa-3x"></i>
+    <i class="fab fa-gulp fa-3x"></i>
+    <i class="fab fa-grunt fa-3x"></i>
+    <i class="fab fa-npm fa-3x"></i>
+  </div>
+  <p class="lead">Workflow</p>
+  <ul>
+    <li><i class="fas fa-check"></i>Mobile-First, Responsive Design</li>
+    <li><i class="fas fa-check"></i>Cross Browser Testing & Debugging</li>
+    <li><i class="fas fa-check"></i>Cross Functional Teams</li>
+    <li><i class="fas fa-check"></i>Agile Development & Scrum</li>
+  </ul>
+</section>
 </template>
 
 <style lang="scss" scoped>
