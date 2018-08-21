151f3d724c10819876e6254b3e6a05cc9f69aa48
ImMyst <titouan.galvani@laposte.net>
2018-08-21 23:53:46 +0200

    Add markup for Awards page

--- pages/awards/index.vue
+++ pages/awards/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <h2>Awards</h2>
+<section class="awards">
+  <h2>Awards & Certifications</h2>
+  <ul>
+    <li><i class="fa fa-trophy"></i>Google Analytics Certified Developer</li>
+    <li><i class="fa fa-trophy"></i>Mobile Web Specialist - Google Certification</li>
+    <li><i class="fa fa-trophy"></i>1<sup>st</sup> Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
+    <li><i class="fa fa-trophy"></i>1<sup>st</sup> Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
+    <li><i class="fa fa-trophy"></i>2<sup>nd</sup> Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
+    <li><i class="fa fa-trophy"></i>1<sup>st</sup> Place - James Buchanan High School - Hackathon 2006</li>
+    <li><i class="fa fa-trophy"></i>3<sup>rd</sup> Place - James Buchanan High School - Hackathon 2005</li>
+  </ul>
+</section>
 </template>
 
 <style lang="scss" scoped>
